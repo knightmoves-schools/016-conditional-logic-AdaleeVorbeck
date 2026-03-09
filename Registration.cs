c4fef05fe62db00cf3b427ae4e4b4c00ab60728c
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-03-09 19:13:31 +0000

    SEE THE LINE AS IT SHINES ON THE SEA

--- Registration.cs
+++ Registration.cs
@@ -2,4 +2,16 @@
 public class Registration
 {
     // Add your code here
+    public bool oldEnough;
+    public string ValidateAge(bool oldEnough) {
+        if (oldEnough == true) {
+            return "old enough";
+        }
+        else {
+            return "";
+            }
+    }
 }
+
+//should contain a method named ValidateAge that takes a parameter named oldEnough and returns "old enough" if oldEnough is true
+//should return an empty string ("") from the ValidateAge method if oldEnough is false
